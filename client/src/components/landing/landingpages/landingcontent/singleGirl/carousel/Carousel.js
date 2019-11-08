fb3878fffd31a6879a98102fbbfedd53fc57032a
Machiavellii <vjekoslav.seferovic@gmail.com>
2019-11-08 17:34:25 +0100

    .

--- client/src/components/landing/landingpages/landingcontent/singleGirl/carousel/Carousel.js
+++ client/src/components/landing/landingpages/landingcontent/singleGirl/carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 // import FsLightbox from 'fslightbox-react';
 
@@ -21,30 +21,38 @@ const Carousel = ({ photos }) => {
     setToggler(!togg);
   };
 
+  console.log(photos.length);
+
   return (
-    <div className="carousel-holder">
-      <div className="carousel-gallery" id="move">
-        {photos.map((photo, i) => (
-          <img
-            key={i}
-            src={`${window.location.origin}/${photo}`}
-            alt=""
-            className="img-fluid"
-            onClick={e => toggler(photo)}
-          />
-        ))}
-      </div>
-      <div className="leftArrow arrow" onClick={leftArrow}>
-        <i className="fas fa-arrow-left"></i>
-      </div>
-      <div className="rightArrow arrow" onClick={rightArrow}>
-        <i className="fas fa-arrow-right"></i>
-      </div>
-      {/* <FsLightbox
-        toggler={toggler}
-        sources={[`${window.location.origin}/${photoGallery}`]}
-      /> */}
-    </div>
+    <Fragment>
+      {photos.length < 1 ? (
+        ''
+      ) : (
+        <div className="carousel-holder">
+          <div className="carousel-gallery" id="move">
+            {photos.map((photo, i) => (
+              <img
+                key={i}
+                src={`${window.location.origin}/${photo}`}
+                alt=""
+                className="img-fluid"
+                onClick={e => toggler(photo)}
+              />
+            ))}
+          </div>
+          <div className="leftArrow arrow" onClick={leftArrow}>
+            <i className="fas fa-arrow-left"></i>
+          </div>
+          <div className="rightArrow arrow" onClick={rightArrow}>
+            <i className="fas fa-arrow-right"></i>
+          </div>
+          {/* <FsLightbox
+      toggler={toggler}
+      sources={[`${window.location.origin}/${photoGallery}`]}
+    /> */}
+        </div>
+      )}
+    </Fragment>
   );
 };
 
