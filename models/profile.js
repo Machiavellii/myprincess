cce9bf7cee88fa73d5f24af8cae824f259be77f7
Machiavellii <vjekoslav.seferovic@gmail.com>
2020-01-21 15:15:16 +0100

    .

--- models/profile.js
+++ models/profile.js
@@ -144,8 +144,6 @@ const ProfileSchema = new Schema({
 ProfileSchema.pre("save", async function(next) {
   const loc = await geocoder.geocode(this.address);
 
-  // console.log(loc);
-
   this.location = {
     type: "Point",
     coordinates: [loc[0].longitude, loc[0].latitude],
