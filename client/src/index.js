b0e70a510fc3ae178b850953bf93b15166d1ca68
Machiavellii <vjekoslav.seferovic@gmail.com>
2020-05-22 20:16:52 +0200

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,12 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './i18next';
-import Spinner from './components/layout/Spinner';
-
-import * as serviceWorker from './serviceWorker';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./i18next";
+import Spinner from "./components/layout/Spinner";
 
 ReactDOM.render(
   <Suspense fallback={<Spinner />}>
     <App />
   </Suspense>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-serviceWorker.register();
