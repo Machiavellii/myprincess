ff506ff0b1b06b21732ddc6fd101f93f6dce3a83
Machiavellii <vjekoslav.seferovic@gmail.com>
2020-05-22 20:03:38 +0200

    .

--- client/src/components/pages/WebcamGirls.js
+++ client/src/components/pages/WebcamGirls.js
@@ -1,75 +1,85 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { getProfiles } from '../../actions/profile';
-import { Link } from 'react-router-dom';
+import React, { useEffect, Fragment } from "react";
+import { connect } from "react-redux";
+import { getProfiles } from "../../actions/profile";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
 
 const WebcamGirls = ({ profiles: { profiles }, getProfiles }) => {
   useEffect(() => {
     getProfiles();
   }, [getProfiles]);
 
-  const webcamGirls = profiles.filter(profile => {
+  const webcamGirls = profiles.filter((profile) => {
     if (profile.webcamlink) {
       return profile;
     }
   });
 
+  console.log(webcamGirls);
+
   return (
     <div className="container">
       {webcamGirls ? (
-        <h4 className="text-center contact-form-heading mt-5">
-          Best Webcam girls available
-        </h4>
-      ) : null}
-      <div className="row">
-        {!webcamGirls ? (
-          <h4 className="text-center no-annoucments-heading">
-            For now, no webcam girls available.
-          </h4>
-        ) : (
-          webcamGirls.map(profile => (
-            <div className="col-sm-6 col-md-4 col-lg-3 mt-5" key={profile._id}>
-              <article className="content">
-                <div className="contentHolder">
-                  <div className="top-holder">
-                    <Link to="/" className="links">
-                      {profile.location.canton}
-                    </Link>
-                  </div>
-                  <Link to={`/profile/user/${profile.user._id}`}>
-                    <img
-                      src={`https://myprincess.jcloud.ik-server.com/${profile.cover_photo}`}
-                      alt=""
-                      className="img-fluid"
-                    />
-                    {!profile.is_active ? (
-                      <div className="inactive">
-                        <h6>This announcement is currently inactive</h6>
+        <div className="container">
+          <div className="row">
+            {webcamGirls.length > 0 ? (
+              webcamGirls.map((profile) => (
+                <Fragment>
+                  <h4 className="text-center contact-form-heading mt-5">
+                    Best Webcam girls available
+                  </h4>
+                  <div
+                    className="col-sm-6 col-md-4 col-lg-3 mt-5"
+                    key={profile._id}
+                  >
+                    <article className="content">
+                      <div className="contentHolder">
+                        <div className="top-holder">
+                          <Link to="/" className="links">
+                            {profile.location ? profile.location.canton : ""}
+                          </Link>
+                        </div>
+                        <Link to={`/profile/user/${profile.user._id}`}>
+                          <img
+                            src={`/${profile.cover_photo}`}
+                            alt=""
+                            className="img-fluid"
+                          />
+                          {!profile.is_active ? (
+                            <div className="inactive">
+                              <h6>This announcement is currently inactive</h6>
+                            </div>
+                          ) : (
+                            <div></div>
+                          )}
+                        </Link>
+                        <div className="bottom-holder">
+                          <h5>
+                            <Link to="/" className="links link-name">
+                              {" "}
+                              {profile.user.nickname}
+                            </Link>
+                          </h5>
+                        </div>
                       </div>
-                    ) : (
-                      <div></div>
-                    )}
-                  </Link>
-                  <div className="bottom-holder">
-                    <h5>
-                      <Link to="/" className="links link-name">
-                        {' '}
-                        {profile.user.nickname}
-                      </Link>
-                    </h5>
+                    </article>
                   </div>
-                </div>
-              </article>
-            </div>
-          ))
-        )}
-      </div>
+                </Fragment>
+              ))
+            ) : (
+              <h4 className="text-center contact-form-heading mt-5">
+                No Webcam girls available
+              </h4>
+            )}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  profiles: state.profile
+const mapStateToProps = (state) => ({
+  profiles: state.profile,
 });
 
 export default connect(mapStateToProps, { getProfiles })(WebcamGirls);
